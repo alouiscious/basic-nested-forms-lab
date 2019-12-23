2769aa0994e9282151ec046f501dd8cde416e468
Alouiscious Media <info@reneesoul.com>
2019-12-23 05:42:43 -0800

    init git

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,17 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def recipe_title=(title)
+  #   self.title = Title.find_or_create_by(title: title)
+  # end
+
+  # def recipe_title
+  #   title.try(:title)    
+  # end
+
+  # def ingredient_attributes=(ingredient)
+  #   self.ingredient = Ingredient.find_or_create_by(name: artist[:name])
+  #   self.ingredient.update(ingredient)    
+  # end
 end
